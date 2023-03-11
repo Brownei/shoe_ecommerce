bcb36dbddb11abe61e6bb1a09005c24830921869
Brownson Esiti <esitibrownson@gmail.com>
2023-03-11 18:28:17 +0100

    Extra changes to the User interface and experience

---
+++ maroon-echidna/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'fg7k9xkg',
+    dataset: 'production'
+  }
+})
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,6 @@ const Navbar = () => {
     useEffect(()=> {
         tl
         .fromTo('.mobile-navbar', 0.5, {x: -50}, {x: 0})
-        .fromTo('.cart', 0.5, {x: 50}, {x: 0})
     })
 
   return (
@@ -31,12 +30,14 @@ const Navbar = () => {
                     ? <RxCross1 fontSize={22} className='cursor-pointer' onClick={() => setToggleMenu(false)}/>
                     : <AiOutlineMenu fontSize={22} className='cursor-pointer' onClick={() => setToggleMenu(true)}/>
                 }
+
+
+                {/* MOBILE NAVBAR SECTION */}
+
                 {toggleMenu && 
                     <div className="mobile-navbar relative">
                        <div className="fixed z-40 bg-[#F6F6F6] -top-12 -left-6 pt-24 h-screen w-[60vw]">
-                            <div>
                                 <RxCross1 fontSize={22} className='cursor-pointer absolute my-4 top-3 right-5' onClick={() => setToggleMenu(false)} />
-                            </div>
                             <div>
                                 <ul className="flex flex-col items-center space-y-10">
                                     <li className="font-semibold hover:scale-110 active:scale-110"><NavLink to= '/'>Home</NavLink></li>
@@ -54,6 +55,9 @@ const Navbar = () => {
                     : ''
                 }
             </div>
+
+                {/* DESKTOP NAVBAR SECTION */}
+
             <div className="font-medium">
                 <NavLink to='/'>s<span className="text-lg font-bold">H</span>oes.</NavLink>
             </div>
@@ -66,6 +70,11 @@ const Navbar = () => {
                     <li className=" hover:scale-110 active:scale-110"><NavLink to= '/about'>About</NavLink></li>
                 </ul>
             </div>
+
+
+
+            {/* CART-SECTION */}
+
             <div className="flex space-x-3 md:space-x-7">
                 <NavLink to='/favorite'><AiOutlineHeart fontSize={22} /></NavLink>
                 <NavLink to='/profile'><CgProfile fontSize={22} /></NavLink>
@@ -76,24 +85,31 @@ const Navbar = () => {
                     }
                     { toggleCart &&
                         <div className="cart relative cursor-default">
-                            <ul className="flex flex-col px-4 py-10 justify-start shadow-sm overflow-auto items-start space-y-10 fixed z-10 bg-[#F6F6F6] top-0 -right-5 pt-24 h-screen w-[70vw] md:w-[40vw]">
-                               <li>
-                                    <CartList image= {"https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/852c5323-cf29-435d-919d-2936c4bce051/air-jordan-5-x-dj-khaled-mens-shoes-F3jrrH.png"} title={"Air Jordan 5 x DJ Khaled"} price={"50,000"} />
-                               </li>
-                               <li>
-                                    <CartList image= {"https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/fadf1b0c-c687-4e3d-8f4e-6d089d115a0c/air-jordan-13-retro-shoe-08RC9v.png"} title={"Air Jordan 13 Retro"} price={"50,000"} />
-                               </li>
-                               <li>
-                                    <CartList image= {"https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/fadf1b0c-c687-4e3d-8f4e-6d089d115a0c/air-jordan-13-retro-shoe-08RC9v.png"} title={"Air Jordan 13 Retro"} price={"50,000"} />
-                               </li>
-                               <li>
-                                    <CartList image= {"https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/fadf1b0c-c687-4e3d-8f4e-6d089d115a0c/air-jordan-13-retro-shoe-08RC9v.png"} title={"Air Jordan 13 Retro"} price={"50,000"} />
-                               </li>
-                                <li className="absolute bottom-20 left-24 md:left-[40%]">
-                                    <button className="px-7 py-2 md:px-10 bg-black text-white hover:bg-[#7C5A3F] rounded-full">Contact Supplier</button>
-                                </li>
-                            </ul>
+                           <div className="overflow-auto fixed z-10 bg-[#F6F6F6] top-0 -right-5 pt-24 h-screen w-[80vw] md:w-[40vw] duration-300">
+                                    <RxCross1 fontSize={22} className='cursor-pointer fixed my-4 top-3 right-5 ' onClick={() => setToggleCart(false)} />
+                                <ul className="flex flex-col px-4 py-10 justify-start shadow-sm items-start space-y-10">
+                                    <li>
+                                            <CartList image= {"https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/852c5323-cf29-435d-919d-2936c4bce051/air-jordan-5-x-dj-khaled-mens-shoes-F3jrrH.png"} title={"Air Jordan 5 x DJ Khaled"} price={"50,000"} />
+                                    </li>
+                                    <li>
+                                            <CartList image= {"https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/fadf1b0c-c687-4e3d-8f4e-6d089d115a0c/air-jordan-13-retro-shoe-08RC9v.png"} title={"Air Jordan 13 Retro"} price={"50,000"} />
+                                    </li>
+                                    <li>
+                                            <CartList image= {"https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/fadf1b0c-c687-4e3d-8f4e-6d089d115a0c/air-jordan-13-retro-shoe-08RC9v.png"} title={"Air Jordan 13 Retro"} price={"50,000"} />
+                                    </li>
+                                    <li>
+                                            <CartList image= {"https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/fadf1b0c-c687-4e3d-8f4e-6d089d115a0c/air-jordan-13-retro-shoe-08RC9v.png"} title={"Air Jordan 13 Retro"} price={"50,000"} />
+                                    </li>
+                                    <li className="sticky bottom-12 md:left-[50%] translate-x-2/4 translate-y-1/2">
+                                        <button className="px-7 py-2 md:px-10 bg-black text-white hover:bg-[#7C5A3F] rounded-full">Contact Supplier</button>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
+                    }
+                    {toggleCart
+                        ? (<div className="bg-black/80 fixed w-full h-screen -z-10 top-0 left-0"></div>)
+                        : ''
                     }
                 </div>
             </div>
